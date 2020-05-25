f7f2d8c233927a779b00d05cc52d9941ca4c7a82
olivakar <oliva.kar@microsoft.com>
2020-05-25 01:01:58 -0700

    crypto certificate creation

--- scripts/create_x509_chain_crypto.py
+++ scripts/create_x509_chain_crypto.py
@@ -10,7 +10,6 @@ import os
 import argparse
 import getpass
 
-# one_day = datetime.timedelta(1, 0, 0)
 PUBLIC_EXPONENT = 65537
 EXTENSION_NAME = ".pem"
 COMMON_DEVICE_PASSWORD_FILE = "demoCACrypto/private/device_key"
@@ -25,7 +24,7 @@ def create_certificate_chain(
     device_password,
     device_count=1,
     key_size=4096,
-    days=365,
+    days=3650,
 ):
     """
     This method will create a basic 3 layered chain certificate containing a root, then an intermediate and then some number of leaf certificates.
@@ -42,18 +41,44 @@ def create_certificate_chain(
     :param days: The number of days for which the certificate is valid. The default is 1 year or 365 days.
     For the root cert this value is multiplied by 10. For the device certificates this number will be divided by 10.
     """
-    # common_name_for_root = "root" + common_name
-    # create_root_ca_cert(
-    #     root_common_name=common_name_for_root, ca_password=ca_password, key_size=key_size, days=days * 10
-    # )
-    pass
+    root_password_file = "demoCACrypto/private/ca_key.pem"
+    root_private_key = create_private_key(
+        password_file=root_password_file, password=ca_password, key_size=key_size
+    )
+    root_cert = create_root_ca_cert(
+        root_common_name="root" + common_name, root_private_key=root_private_key, days=days
+    )
+
+    intermediate_password_file = "demoCACrypto/private/intermediate_key.pem"
+
+    intermediate_private_key = create_private_key(
+        password_file=intermediate_password_file, password=intermediate_password, key_size=key_size
+    )
+
+    intermediate_cert = create_intermediate_ca_cert(
+        root_cert=root_cert,
+        root_key=root_private_key,
+        intermediate_common_name="inter" + common_name,
+        intermediate_private_key=intermediate_private_key,
+        days=days,
+    )
+
+    create_multiple_device_keys_and_certs(
+        number_of_devices=device_count,
+        inter_cert=intermediate_cert,
+        inter_key=intermediate_private_key,
+        device_common_name="device" + common_name,
+        password=device_password,
+        key_size=key_size,
+        days=days,
+    )
 
 
 def create_private_key(password_file, password=None, key_size=4096):
     if password:
         encrypt_algo = serialization.BestAvailableEncryption(str.encode(password))
     else:
-        encrypt_algo = None
+        encrypt_algo = serialization.NoEncryption()
 
     private_key = rsa.generate_private_key(
         public_exponent=PUBLIC_EXPONENT, key_size=key_size, backend=default_backend()
@@ -98,12 +123,7 @@ def create_root_ca_cert(root_common_name, root_private_key, days=3650):
 
 
 def create_intermediate_ca_cert(
-    root_cert,
-    root_key,
-    intermediate_common_name,
-    intermediate_private_key,
-    key_size=4096,
-    days=3650,
+    root_cert, root_key, intermediate_common_name, intermediate_private_key, days=3650
 ):
     file_intermediate_certificate = "demoCACrypto/newcerts/intermediate_cert.pem"
     file_intermediate_csr = "demoCACrypto/newcerts/intermediate_csr.pem"
@@ -132,13 +152,14 @@ def create_intermediate_ca_cert(
     return intermediate_cert
 
 
-def create_cert_builder(subject, issuer_name, public_key, days, is_ca=False):
+def create_cert_builder(subject, issuer_name, public_key, days=30, is_ca=False):
     builder = x509.CertificateBuilder()
 
     builder = builder.subject_name(subject)
     builder = builder.issuer_name(issuer_name)
     builder = builder.public_key(public_key)
     builder = builder.not_valid_before(datetime.today())
+
     builder = builder.not_valid_after(datetime.today() + timedelta(days=days))
     builder = builder.serial_number(int(uuid.uuid4()))
     builder = builder.add_extension(
@@ -242,9 +263,10 @@ def before_cert_creation_from_pipeline():
     #     print("Configuration file have NOT been copied")
 
 
-def create_verficattion_cert(nonce, root_verify):
+def create_verification_cert(nonce, issuer_password, root_verify=False, key_size=4096):
     issuer_private_key = None
     issuer_cert = None
+    encoded_issuer_password = str.encode(issuer_password)
 
     if root_verify:
         verification_password_file = "demoCACrypto/private/verfiication_ca_key.pem"
@@ -259,11 +281,13 @@ def create_verficattion_cert(nonce, root_verify):
         issuer_key_file = "demoCACrypto/private/intermediate_key.pem"
         issuer_cert_file = "demoCACrypto/newcerts/intermediate_cert.pem"
 
-    with open(issuer_key_file, "r") as fh:
-        pem_data = fh.read()
-        issuer_private_key = x509.load_pem_x509_certificate(pem_data, default_backend())
+    with open(issuer_key_file, "rb") as key_file:
+        pem_data = key_file.read()
+        issuer_private_key = serialization.load_pem_private_key(
+            pem_data, password=encoded_issuer_password, backend=default_backend()
+        )
 
-    with open(issuer_cert_file, "r") as fh:
+    with open(issuer_cert_file, "rb") as fh:
         pem_data = fh.read()
         issuer_cert = x509.load_pem_x509_certificate(pem_data, default_backend())
 
@@ -277,11 +301,10 @@ def create_verficattion_cert(nonce, root_verify):
         is_ca=False,
     )
 
-    verification_builder = create_cert_builder_for_verification(
+    verification_builder = create_cert_builder(
         subject=verification_csr.subject,
         issuer_name=issuer_cert.subject,
         public_key=verification_csr.public_key(),
-        is_ca=False,
     )
 
     verification_cert = verification_builder.sign(
@@ -291,133 +314,148 @@ def create_verficattion_cert(nonce, root_verify):
         f.write(verification_cert.public_bytes(serialization.Encoding.PEM))
 
 
-def create_cert_builder_for_verification(subject, issuer_name, public_key, is_ca=False):
-    builder = x509.CertificateBuilder()
-
-    builder = builder.subject_name(subject)
-    builder = builder.issuer_name(issuer_name)
-    builder = builder.public_key(public_key)
-    # builder = builder.not_valid_before(datetime.today())
-    # builder = builder.not_valid_after(datetime.today() + timedelta(days=days))
-    builder = builder.serial_number(int(uuid.uuid4()))
-    builder = builder.add_extension(
-        x509.BasicConstraints(ca=is_ca, path_length=None), critical=True
-    )
-    return builder
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a certificate chain.")
     parser.add_argument("domain", help="Domain name or common name.")
-    #
-    # parser.add_argument(
-    #     "--ca-password", type=str, help="CA key password. If omitted it will be prompted."
-    # )
-    #
+    parser.add_argument(
+        "-s",
+        "--key-size",
+        type=int,
+        help="Size of the key in bits. 2048 bit is quite common. "
+        + "4096 bit is more secure and the default.",
+    )
+    parser.add_argument(
+        "-d",
+        "--days",
+        type=int,
+        help="Validity time in days. Default is 10 years for root , 1 year for intermediate and 1 month for leaf",
+    )
+    parser.add_argument(
+        "--ca-password", type=str, help="CA key password. If omitted it will be prompted."
+    )
+    parser.add_argument(
+        "--intermediate-password",
+        type=str,
+        help="intermediate key password. If omitted it will be prompted.",
+    )
+    parser.add_argument(
+        "--device-password", type=str, help="device key password. If omitted it will be prompted."
+    )
+
+    parser.add_argument(
+        "--issuer-password",
+        type=str,
+        help="The passphrase for the issuer needed only during verification. "
+        "If omitted it will be prompted.",
+    )
+
+    parser.add_argument(
+        "--device-count", type=str, help="Number of devices that present in a group. Default is 1."
+    )
 
     parser.add_argument(
         "--mode",
         type=str,
-        help="The mode in which certificate is created. By default non-verification mode. For verification use 'verification'",
+        help="The mode in which certificate is created. "
+        "By default non-verification mode. For verification use 'verification'",
     )
-
-    parser.add_argument(
-        "--root-verify",
-        type=str,
-        help="The boolean value to enter in case it is the root or intermediate verification. By default it is True meaning root verifictaion. If veriication of intermediate certification is needed please enter False ",
-    )
-
     parser.add_argument(
         "--nonce",
         type=str,
-        help="thumprint generated from iot hub certificates. During verification mode if omitted it will be prompted.",
+        help="Thumbprint generated from IoT Hub or DPS certificates for performing proof of possession. "
+        "During verification mode if omitted it will be prompted.",
     )
-
+    parser.add_argument(
+        "--root-verify",
+        type=str,
+        help="The boolean value to enter to mean either the root or intermediate verification. "
+        "By default it is True meaning root verifictaion. "
+        "If veriication of intermediate certification is needed please enter False.",
+    )
     args = parser.parse_args()
 
     common_name = args.domain
 
+    if args.key_size:
+        key_size = args.key_size
+    else:
+        key_size = 4096
+    if args.days:
+        days = args.days
+    else:
+        days = 30
+
+    ca_password = None
+    intermediate_password = None
     if args.mode:
         if args.mode == "verification":
             mode = "verification"
+            print("In verification mode...........")
         else:
             raise ValueError(
                 "No other mode except verification is accepted. Default is non-verification"
             )
     else:
         mode = "non-verification"
+        print("In non-verification mode...........")
 
     if mode == "non-verification":
-        pass
+        print("In non-verification mode...........")
+        if args.ca_password:
+            ca_password = args.ca_password
+        else:
+            ca_password = getpass.getpass("Enter pass phrase for root key: ")
+        if args.intermediate_password:
+            intermediate_password = args.intermediate_password
+        else:
+            intermediate_password = getpass.getpass("Enter pass phrase for intermediate key: ")
+        if args.device_password:
+            device_password = args.device_password
+        else:
+            device_password = getpass.getpass("Enter pass phrase for device key: ")
+        if args.device_count:
+            device_count = args.device_count
+        else:
+            device_count = 1
     else:
+        print("In verification mode...........")
         if args.nonce:
             nonce = args.nonce
+            print("Received nonce")
         else:
             nonce = getpass.getpass("Enter nonce for verification mode")
-
-        # By default root verify is True unless specifically passed
         if args.root_verify:
             lower_root_verify = args.root_verify.lower()
+
             if lower_root_verify == "false":
                 root_verify = False
+                print("Root verify is False. So will be verifying intermediate certificate")
             else:
                 root_verify = True
+                print("Root verify is True. So will be verifying root certificate")
         else:
-            root_verify = True
+            print("Root verify will be by default False.")
+            root_verify = False
 
-    # if args.ca_password:
-    #     ca_password = args.ca_password
-    # else:
-    #     ca_password = getpass.getpass("Enter pass phrase for root key: ")
+        if args.issuer_password:
+            issuer_password = args.issuer_password
+        else:
+            issuer_password = getpass.getpass(
+                "Enter pass phrase for issuer certificate verification: "
+            )
 
-    # common_name = "leviosa"
-    # print(common_name.decode("utf-8"))
-    # bytes_val = str.encode(common_name)
-    # print(bytes_val)
-    # u_val = bytes_val.decode("utf-8")
-    # print(u_val)
-    # print(str.encode(common_name).decode("utf-8"))
-
-    if mode == "non-verification":
-        create_verficattion_cert(nonce, root_verify)
+    if os.path.exists("demoCACrypto/private/") and os.path.exists("demoCACrypto/newcerts/"):
+        print("demoCA already exists.")
     else:
-
         create_directories_and_prereq_files(False)
-        key_size = 4096
-        abs_common_name = "leviosa"
-        root_pass = "hogwarts"
-        inter_pass = "hogwartsi"
-        device_pass = "hogwartsd"
 
-        root_password_file = "demoCACrypto/private/ca_key.pem"
-        root_private_key = create_private_key(
-            password_file=root_password_file, password=root_pass, key_size=key_size
-        )
-        root_cert = create_root_ca_cert(
-            root_common_name="root" + abs_common_name, root_private_key=root_private_key, days=3650
-        )  # Arg parse password
-
-        intermediate_password_file = "demoCACrypto/private/intermediate_key.pem"
-
-        intermediate_private_key = create_private_key(
-            password_file=intermediate_password_file, password=inter_pass, key_size=key_size
-        )
-
-        intermediate_cert = create_intermediate_ca_cert(
-            root_cert=root_cert,
-            root_key=root_private_key,
-            intermediate_common_name="inter" + abs_common_name,
-            intermediate_private_key=intermediate_private_key,
-            key_size=4096,
-            days=3650,
-        )
-
-        create_multiple_device_keys_and_certs(
-            number_of_devices=3,
-            inter_cert=intermediate_cert,
-            inter_key=intermediate_private_key,
-            device_common_name="device" + abs_common_name,
-            password=device_pass,
-            key_size=4096,
-            days=3650,
+    if mode == "verification":
+        create_verification_cert(nonce, issuer_password, root_verify, key_size=4096)
+    else:
+        create_certificate_chain(
+            common_name=args.domain,
+            ca_password=ca_password,
+            intermediate_password=intermediate_password,
+            device_password=device_password,
+            device_count=int(device_count),
         )
